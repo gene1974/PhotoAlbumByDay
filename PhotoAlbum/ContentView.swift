197e41f0c0d509411b5b8c78d0185bc89fb47f9a
gene1974 <842491684@qq.com>
2026-03-21 09:38:23 +0800

    Initial Commit

---
+++ PhotoAlbum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoAlbum
+//
+//  Created by Yingzhuo Huang on 2026/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoAlbum/PhotoAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAlbumApp.swift
+//  PhotoAlbum
+//
+//  Created by Yingzhuo Huang on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
